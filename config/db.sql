e1205dc2273a36b4a98b121b08c5ac162ea70e8a
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-10-06 13:54:25 -0600

    fix: changed price to 100

--- config/db.sql
+++ config/db.sql
@@ -39,7 +39,7 @@ CREATE TABLE comunidades (
     contacto VARCHAR(50),
     direccion TEXT,
     idRuta INT NULL, -- Permite NULL al borrar la ruta
-    costoPaquete DECIMAL(10, 2) DEFAULT 170.00,
+    costoPaquete DECIMAL(10, 2) DEFAULT 100.00,
     notas VARCHAR(100),
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
